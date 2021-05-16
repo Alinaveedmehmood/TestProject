227fae9641cb75e18ea408429589acb0ecf3d9e6
Alinaveedmehmood <Ali.Mehmood@unioncoop.ae>
2021-05-16 12:20:05 +0400

    my First Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by ALI on 16/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
